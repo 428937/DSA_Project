c45b01c75f2cf8bc8ea2e5456953dd83d72a7b88
428937 <alpkaanayas@gmail.com>
2026-07-02 00:39:27 +0300

    Update VeriYukleme.java

--- src/yonetici/VeriYukleme.java
+++ src/yonetici/VeriYukleme.java
@@ -5,7 +5,7 @@ import models.BaglıListe;
 import java.io.*;
 import java.util.StringTokenizer;
 
-// Veri Yükleme ve Kaydetme işlemleri için
+// Veri Yükleme ve Kaydetme işlemleri
 public class VeriYukleme {
     private static final String VERI_DOSYASI = "data/ilanlar.txt";
     
